b6cb43828c0f2f5e53fa7ead8e69eeff892757b4
anil6111 <128017019+anil6111@users.noreply.github.com>
2023-04-05 11:45:51 +0530

    Update census.py

--- census.py
+++ census.py
@@ -55,7 +55,7 @@ if uploaded_file is not None:
         st.write(f"Total population of {selected_state} in 2011: {state_pop_data.iloc[0]['Population']:,}")
     if st.checkbox("pie chart of female workers vs male workers :"):
         fig,ax=plt.subplots()
-        x=np.array(data['State_name']==GUJARAT.value_counts(),data['State_name']==ORISSA.value_counts(),data['State_name']==ANDHRA PRADESH.value_counts())
+        x=np.array(data['State_name']==BIHAR.value_counts(),data['State_name']==ORISSA.value_counts(),data['State_name']==KERALA.value_counts())
         mylabels=['GUJARAT','ORISSA','ANDHRA PRADESH']
         sns.pie(x,labels=mylabels)
         st.pyplot(fig)
