514e1dedae79b6cc8a92694d9dbe09d3d0b52349
anil6111 <128017019+anil6111@users.noreply.github.com>
2023-04-06 01:26:41 +0530

    Add files via upload

--- census.py
+++ census.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
 import streamlit as st
 import random
 from PIL import Image
